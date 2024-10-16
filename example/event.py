7cc492548106b8b4f8e1ad9adcbcd504440f9ec1
Trần Bá Hiến <69721832+tranhien1612@users.noreply.github.com>
2024-10-16 16:04:18 +0700

    Update event.py

--- example/event.py
+++ example/event.py
@@ -7,15 +7,18 @@ def click_and_crop(event, x, y, flags, param):
 		refPt = [(x, y)]
 	elif event == cv2.EVENT_LBUTTONUP:
 		refPt.append((x, y))
-		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+		cv2.rectangle(clone, refPt[0], refPt[1], (0, 255, 0), 2)
 		
 image = cv2.imread("../images/car_2.jpg")
+clone = image.copy()
 cv2.namedWindow("image")
 cv2.setMouseCallback("image", click_and_crop)
 
 while True:
-	cv2.imshow("image", image)
+	cv2.imshow("image", clone)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
-cv2.destroyAllWindows()
+	if cv2.waitKey(1) & 0xFF == ord('x'): #delete all rectangle
+		clone = image.copy()
 
+cv2.destroyAllWindows()
